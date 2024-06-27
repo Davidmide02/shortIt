69a67335b9cccb5cc8e0c80809ab41b21c5d367b
Davidmide02 <Davidmide07@gmail.com>
2024-06-27 17:00:43 +0100

    add form

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const express = require("express")
+const express = require("express");
 
-const app= express()
-app.set("view engine","ejs")
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res)=>{
-res.render('index')
-})
+app.get("/", (req, res) => {
+  // console.log(req.body.url);
+  res.render("index");
+});
 
-app.listen(process.env.PORT || 3000)
+app.post("/", (req, res) => {
+  //    console.log( "body",req.body)
+  console.log(req.body.url);
+
+  //    res.render(`Hello ${req.body}`)
+});
+
+app.listen(process.env.PORT || 3000);
